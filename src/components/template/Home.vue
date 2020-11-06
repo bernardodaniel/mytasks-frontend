84c649e5d2832537d20e87f0d6ff5394a1f459c9
Daniel Petrico <daniel@danielpetrico.com>
2020-11-06 17:51:07 -0300

    layout

---
+++ src/components/template/Home.vue
@@ -0,0 +1,61 @@
+<template>
+  <div>
+    <v-row cols="12">
+      <Coluna titulo="Aberto" cor="indigo" :tarefas="tarefasAbertas" />
+
+      <Coluna titulo="Em andamento" cor="primary" :tarefas="tarefasEmAndamento" />
+
+      <Coluna titulo="Concluídas" cor="success" :tarefas="tarefasConcluidas" />
+
+      <Coluna titulo="Canceladas" cor="error" :tarefas="tarefasCanceladas" />
+    </v-row>
+
+    <TarefaDialog />
+  </div>
+</template>
+
+<script>
+import Coluna from "../../components/coluna/Coluna"
+import TarefaDialog from '../tarefa/TarefaDialog'
+
+export default {
+  components: {
+    Coluna, TarefaDialog
+  },
+  data() {
+    return {
+      tarefasAbertas: [
+        {
+          id: 1,
+          titulo: "Tarefa X",
+          status: "ABERTA",
+        },
+      ],
+      tarefasEmAndamento: [
+        {
+          id: 2,
+          titulo: "Tarefa Z",
+          status: "EM_ANDAMENTO",
+        },
+      ],
+      tarefasConcluidas: [
+        {
+          id: 3,
+          titulo: "Tarefa W",
+          status: "CONCLUIDA",
+        },
+      ],
+      tarefasCanceladas: [
+        {
+          id: 4,
+          titulo: "Tarefa Y",
+          status: "CONCLUIDA",
+        },
+      ],
+    };
+  },
+};
+</script>
+
+<style>
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,23 +1,22 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
+import Home from '@/components/template/Home'
+import Categoria from '@/components/categoria/Categoria'
 
 Vue.use(VueRouter)
 
 const routes = [
   {
     path: '/',
-    name: 'Home',
-    component: Home
+    component: Home,
+    name: 'Home'
   },
   {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  }
+    path: '/categoria',
+    component: Categoria,
+    name: 'Categoria'
+  },
+
 ]
 
 const router = new VueRouter({
